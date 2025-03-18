c60c291c29aa445145f1795ff0d89506f776045d
Avidu Witharana <167880662+avidzcheetah@users.noreply.github.com>
2025-03-18 11:05:35 +0530

    Fix Loader issue

---
+++ App.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+//import { Assistant } from "./assistants/openai"; // Uncomment to use openai API
+import { Assistant } from "./assistants/googleai";
+import { Loader } from "./components/Loader/Loader";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
+import styles from "./App.module.css";
+
+function App() {
+  const assistant = new Assistant();
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isStreaming, setIsStreaming] = useState(false);
+
+  function updateLastMessageContent(content) {
+    setMessages((prevMessages) =>
+      prevMessages.map((message, index) =>
+        index === prevMessages.length - 1
+          ? { ...message, content: `${message.content}${content}` }
+          : message
+      )
+    );
+  }
+
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    setIsLoading(true);
+    try {
+      const result = await assistant.chatStream(content);
+      let isFirstChunk = false;
+
+      for await (const chunk of result) {
+        if (!isFirstChunk) {
+          isFirstChunk = true;
+          addMessage({ content: "", role: "assistant" });
+          setIsLoading(false);
+          setIsStreaming(true);
+        }
+
+        updateLastMessageContent(chunk);
+      }
+
+      setIsStreaming(false);
+    
+    } catch (error) {
+      addMessage({
+        content: "Meow... I tried, but it didn’t work. Maybe try again, human? Purrhaps it'll work next time! 🐾",
+        role: "system",
+      });
+      setIsLoading(false);
+      setIsStreaming(false);
+    }
+  }
+
+  return (
+    <div className={styles.App}>
+      {isLoading && <Loader />}
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/work-from-home.png" />
+        <h1 className={styles.Title}>Mew</h1>
+        <h6 className={styles.Title}>The AI Chatbot - by Avidu</h6>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls
+        isDisabled={isLoading || isStreaming}
+        onSend={handleContentSend}
+      />
+    </div>
+  );
+}
+
+export default App;
